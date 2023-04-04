f8555b0cbc12556bb18cb25c729188c6e2144a61
Nikolai Maidan <axroit.08@gmail.com>
2023-04-04 21:26:49 +0400

    add first components

---
+++ src/content/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+
+  return (
+      <div className={'main-div'}>
+        <div className={'test-styles'}>Hello Dima :)</div>
+      </div>
+  );
+}
+
+export default App;
