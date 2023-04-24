44adf7cb7fe0ffbe6b42894a2ba0e27c09858bc2
Nikolai Maidan <axroit.08@gmail.com>
2023-04-24 22:31:54 +0800

    added files for Anakin

---
+++ src/content/components/Anakin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DarthVader() {
+
+  return (
+      <div className={'darth-vader'}>
+        Hello I am Anakin Skywalker
+      </div>
+  );
+}
+
+export default DarthVader;
